1179cb418302f3963262f5d74e344298aa932064
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-21 13:51:56 +0200

    *

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,8 @@
-// vite.config.ts
 import { defineConfig } from "vitest/config";
-
+import tsconfigPaths from "vite-tsconfig-paths";
 export default defineConfig({
   test: {
     globals: true,
   },
+  plugins: [tsconfigPaths()],
 });
