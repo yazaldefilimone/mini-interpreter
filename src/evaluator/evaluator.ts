0227545e15b01749771eb75de01c4fcdddcb9927
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-24 22:49:47 +0200

    *

--- src/evaluator/evaluator.ts
+++ src/evaluator/evaluator.ts
@@ -1,4 +1,4 @@
-import { Environment, globalEnvironment } from "environment";
+import { Environment, globalEnvironment } from 'environment';
 
 type expType<T = any | unknown> = T;
 
@@ -18,11 +18,11 @@ export class Evaluator {
     }
 
     // variables
-    if (exp.at(0) === "var") {
+    if (exp.at(0) === 'var') {
       const [_, name, value] = exp;
       return env.define(name, this.eva(value, env));
     }
-    if (exp.at(0) === "set") {
+    if (exp.at(0) === 'set') {
       const [_, name, value] = exp;
       return env.assign(name, this.eva(value, env));
     }
@@ -31,12 +31,12 @@ export class Evaluator {
       return env.lookup(exp);
     }
     // block scope
-    if (exp.at(0) === "begin") {
+    if (exp.at(0) === 'begin') {
       const newEnv = new Environment({}, env);
       return this._evalBlock(exp, newEnv);
     }
     // if
-    if (exp.at(0) === "if") {
+    if (exp.at(0) === 'if') {
       const [_tag, expression, consequent, alternative] = exp;
       if (this.eva(expression, env)) {
         return this.eva(consequent, env);
@@ -44,30 +44,51 @@ export class Evaluator {
       return this.eva(alternative, env);
     }
     // while
-    if (exp.at(0) === "while") {
-      console.log("hei");
-
+    if (exp.at(0) === 'while') {
       const [_tag, condition, body] = exp;
       let result: unknown;
-      console.log({ body, condition, ex: this.eva(condition, env) });
-
       while (this.eva(condition, env)) {
         result = this.eva(body, env);
-        console.log({ body });
       }
       return result;
+    }
+    // functions:user
+    if (exp.at(0) === 'def') {
+      const [_tag, name, params, body] = exp;
+      const fn = {
+        params,
+        body,
+        env,
+      };
+      return env.define(name, fn);
     }
     // functions:native/user
 
     if (Array.isArray(exp)) {
       const fn = this.eva(exp.at(0), env);
       const args = exp.slice(1).map((e) => this.eva(e, env));
-      if (typeof fn === "function") {
+      if (typeof fn === 'function') {
         return fn(...args);
       }
+      // user function
+      const activationRecord = {};
+      fn.params.map((param: any, index: number) => {
+        return (activationRecord[param] = args[index]);
+      });
+      const activationEnv = new Environment(activationRecord, fn.env);
+      return this._evalBody(fn.body, activationEnv);
     }
-    throw `Type Error: ${JSON.stringify(typeof exp === "object" ? exp.at(0) : exp)} unimplemented!`;
+
+    throw `Type Error: ${JSON.stringify(typeof exp === 'object' ? exp.at(0) : exp)} unimplemented!`;
   }
+
+  private _evalBody(body: unknown[], env: Environment) {
+    if (body.at(0) === 'begin') {
+      return this._evalBlock(body, env);
+    }
+    return this.eva(body, env);
+  }
+
   private _evalBlock(exp: expType, env: Environment) {
     let result: unknown;
     const [_tag, ...expressions] = exp;
@@ -76,14 +97,15 @@ export class Evaluator {
     });
     return result;
   }
+
   private _isNumber(exp: expType) {
-    return typeof exp === "number";
+    return typeof exp === 'number';
   }
   private _isString(exp: expType) {
-    return typeof exp === "string" && exp[0] === '"' && exp.at(-1) === '"';
+    return typeof exp === 'string' && exp[0] === '"' && exp.at(-1) === '"';
   }
 
   private _isVariableName(exp: expType) {
-    return typeof exp === "string" && /^[+\-*/<>=a-zA-Z][a-zA-Z)-9_]*$/.test(exp);
+    return typeof exp === 'string' && /^[+\-*/<>=a-zA-Z][a-zA-Z)-9_]*$/.test(exp);
   }
 }
---
+++ src/evaluator/tests/evaluator-functions.spec.ts
@@ -0,0 +1,49 @@
+import { Evaluator } from 'evaluator';
+import { parserCode } from 'tests/main';
+
+const makeSut = () => {
+  const sut = new Evaluator();
+  const evaParser = (code: string) => sut.eva(parserCode(code));
+  return {
+    sut,
+    evaParser,
+  };
+};
+
+describe('Evaluator', () => {
+  it('function: should can be create functions', () => {
+    const { evaParser } = makeSut();
+    const code = `
+      (begin 
+        (def sum (x)
+          (+ x x)
+        )
+        (sum 10)
+      )
+    `;
+    expect(evaParser(code)).toBe(10 + 10);
+  });
+  it('function: should can be possible use clojure functions', () => {
+    const { evaParser } = makeSut();
+    const code = `
+      (begin 
+        (var global 10)
+
+        (def clojure (y x)
+          (begin
+            (var sum (+ y x))
+            (def fnr (n)
+             (+ (+ sum n) global)
+            )
+
+            fnr
+          )
+        )
+
+        (var fn (clojure 1 2))
+        (fn 3)
+      )
+    `;
+    expect(evaParser(code)).toBe(10 + 1 + 2 + 3);
+  });
+});
