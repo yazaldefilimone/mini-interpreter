9432a9c17bad67bbe43f7d7795132ac25394381d
Yazalde Filimone <yazaldefilimon@gmail.com>
2023-05-27 22:36:43 +0200

    *

---
+++ src/evaluator/tests/evaluator-class.spec.ts
@@ -0,0 +1,39 @@
+import { Evaluator } from 'evaluator';
+import { parserCode } from 'tests/main';
+
+const makeSut = () => {
+  const sut = new Evaluator();
+  return {
+    sut,
+  };
+};
+
+describe('Evaluator', () => {
+  it('sort suga expression', () => {
+    const { sut } = makeSut();
+    const code = `
+    (begin 
+      class Math null 
+        (begin
+          (def constructor (this x y)
+            (begin
+              (set (prop this x) x)
+              (set (prop this y) y)
+            )
+          )
+
+          (def calc (this)
+            (+ (prop this x) (prop this y))
+          )
+        )
+    
+      (var math (new Math 10 20))
+
+      ((prop math calc) math)
+    )
+
+    `;
+    const codeParser = parserCode(code);
+    expect(sut.eva(codeParser)).toBe(30);
+  });
+});
